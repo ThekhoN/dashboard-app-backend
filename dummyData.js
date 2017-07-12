8c349e197ca3631313d0ec9d2f9d619012cf8e05
Ngaosathe <thekho.ngaosathe@jasperindia.local>
2017-07-12 18:45:10 +0530

    clean up

--- dummyData.js
+++
@@ -1,8 +0,0 @@
-{
-	"gender":"female",
-	"locale":"en_US",
-	"profilePhoto": "https://n4.sdlcdn.com/imgs/f/s/e/26May_CottonSilk_4H-b5152.jpg",
-	"timezone": "GMT+5.5",
-	"lat":"28.475760",
-	"long": "77.093081"
-}
